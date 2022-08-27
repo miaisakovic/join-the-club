03e53e76a9e95d6457ab5846b0424006c2ab08b9
Mia Isakovic <47527694+miaisakovic@users.noreply.github.com>
2022-08-27 17:42:49 -0400

    Update and rename welcomePage.py to welcome_page.py

--- welcome_page.py
+++ welcome_page.py
@@ -5,11 +5,11 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title ("WELCOME")
 
-welcome_page = ImageTk.PhotoImage(Image.open("welcomePage.png"))
+welcome_page = ImageTk.PhotoImage(Image.open("images/welcome_page.png"))
 welcome = Label(image = welcome_page)
 welcome.pack()
 
-startButton_image = ImageTk.PhotoImage(Image.open ("startButton.png"), Image.ANTIALIAS)
+startButton_image = ImageTk.PhotoImage(Image.open ("images/start_button.png"), Image.ANTIALIAS)
 startButton = Button(root, image = startButton_image, borderwidth=0, height = 86, width = 470)
 startButton.place(x=115, y=470)
 
